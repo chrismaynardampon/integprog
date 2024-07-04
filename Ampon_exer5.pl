aa0032578f1e58426280a8019cd39a6c2eaa6d27
Chris Maynard Ampon <cmdampon@addu.edu.ph>
2024-07-04 11:50:01 +0800

    Update Ampon_exer5.pl

--- Ampon_exer5.pl
+++ Ampon_exer5.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use CGI qw(:standard);
 
-use Inline Java => <<'END',CLASSPATH => 'A:\ADDU\3rd Year\exer4\mysql-connector-java-5.1.47.jar', AUTOSTUDY => 1;
+use Inline Java => <<'END',CLASSPATH => 'C:\webserver\mysql-connector-java-5.1.47.jar', AUTOSTUDY => 1;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -16,7 +16,6 @@ public class Products {
 
 public Products(){
 	initialized();
-	get_database();
 }
 
 public Products (String pid, String pcat, String pdesc, String price, String quantity, String sdate){
@@ -101,7 +100,7 @@ public void insert_category(String category){
         }
     }
 
-public String insertData(){
+public void insertData(){
 	try
 	{
 		String query ="insert into products values('"+this.pid+"','"+this.pcat+"','"+this.pdesc+"','"+this.price+"','"+this.quantity+"','"+this.sdate+"','hatdog')";
@@ -111,10 +110,9 @@ public String insertData(){
 	{
 		System.out.println(ex);
 	}
-	return "Inserted";
 }
 
-public String updateData(){
+public void updateData(){
 	try
 	{
 		if (this.pid == null || this.pid.isEmpty()){this.pid = " ";}
@@ -125,10 +123,9 @@ public String updateData(){
 	{
 		System.out.println(ex);
 	}
-	return "Updated";
 }
 
-public String deleteData(){
+public void deleteData(){
 	try
 	{
 		String query = "delete from products where pid='" + this.pid + "' and pcat='"+this.pcat+"'";
@@ -138,37 +135,29 @@ public String deleteData(){
 	{
 		System.out.println(ex);
 	}
-	return "Deleted";
 }
 
 
 
-public String[][] selectAllProducts(){
-	String [][] resultArray = new String[0][];
-	try
-	{
-		String query = "select * from products";
-		rs = st.executeQuery(query);
-		rs.last();
-		int rowCount = rs.getRow();
-		rs.beforeFirst();
-		resultArray = new String[rowCount][6];
-		int rowindex = 0;
-		while (rs.next()){
-			resultArray[rowindex][0] = rs.getString("pid");
-			resultArray[rowindex][1] = rs.getString("pcat");
-			resultArray[rowindex][2] = rs.getString("pdesc");
-			resultArray[rowindex][3] = rs.getString("price");
-			resultArray[rowindex][4] = rs.getString("quantity");
-			resultArray[rowindex][5] = rs.getString("sdate");
-			rowindex++;
-		}
-	}
-	catch (SQLException ex)
-	{
-		System.out.println("Error executing query: " + ex);
-	}
-	return resultArray;
+public String[][] selectAllProducts() {
+        List<String[]> products = new ArrayList<>();
+        try {
+            String query = "select * from products";
+            rs = st.executeQuery(query);
+            while (rs.next()) {
+                String[] product = new String[6];
+                product[0] = rs.getString("pid");
+                product[1] = rs.getString("pcat");
+                product[2] = rs.getString("pdesc");
+                product[3] = rs.getString("price");
+                product[4] = rs.getString("quantity");
+                product[5] = rs.getString("sdate");
+                products.add(product);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error executing query: " + ex);
+        }
+        return products.toArray(new String[0][0]);
 }
 
 }
@@ -176,29 +165,55 @@ END
 
 my $cgi = CGI->new;
 
-my $action = $cgi->param('action')||'';
-my $pid = $cgi->param('pid')||0;
-my $pcat = $cgi->param('pcat')||'';
-my $pdesc = $cgi->param('pdesc')||'';
-my $price = $cgi->param('price')||'';
-my $quantity = $cgi->param('quantity')||'';
-my $sdate = $cgi->param('sdate')||'';
+my $action = $cgi->param('action') || '';
+my $pid = $cgi->param('pid') || '';
+my $pcat = $cgi->param('pcat') || '';
+my $pdesc = $cgi->param('pdesc') || '';
+my $price = $cgi->param('price') || '';
+my $quantity = $cgi->param('quantity') || '';
+my $sdate = $cgi->param('sdate') || '';
 
 print $cgi->header();
 print $cgi->start_html('Exer 5');
 print $cgi->h1('Exer 5');
 
 print qq(
-	<form method="POST" action="">
-		Product ID: <input type="text" name="product-id" value="$pid"><br>
-		Product Category: <input type="text" name="product-category" value="$pcat"><br>
-		Product Description: <input type="text" name="product-description" value="$pdesc"><br>
-		Price <input type="text" name="price" value="$price"><br>
-		Quantity: <input type="text" name="quantity" value="$quantity"><br>
-		Stock-In Date: <input type="text" name="sdate" value="$sdate"><br>
-		<input type="submit" value="Add">
-		<input type="submit" value="Update">
-		<input type="submit" value="Delete">
-	</form>
-)
+    <form method="POST" action="">
+        Product ID: <input type="text" name="pid" value="$pid"><br>
+        Product Category: <input type="text" name="pcat" value="$pcat"><br>
+        Product Description: <input type="text" name="pdesc" value="$pdesc"><br>
+        Price: <input type="text" name="price" value="$price"><br>
+        Quantity: <input type="text" name="quantity" value="$quantity"><br>
+        Stock-In Date: <input type="text" name="sdate" value="$sdate"><br>
+	<input type="submit" name="action" value="Show Data">
+        <input type="submit" name="action" value="Insert">
+        <input type="submit" name="action" value="Update">
+        <input type="submit" name="action" value="Delete">
+    </form>
+);
 
+print $cgi->end_html;
+
+if ($action eq 'Insert') {
+    my $product = Products->new($pid, $pcat, $pdesc, $price, $quantity, $sdate);
+    print $product->insertData();
+} elsif ($action eq 'Update') {
+    my $product = Products->new($pid, $pcat, $pdesc, $price, $quantity, $sdate);
+    print $product->updateData();
+} elsif ($action eq 'Delete') {
+    my $product = Products->new($pid, $pcat);
+    print $product->deleteData();
+} elsif ($action eq 'Show Data') {
+    my $product = Products->new();
+    my $data = $product->selectAllProducts();
+    print "<table border='1'>";
+    print "<tr><th>Product ID</th><th>Category</th><th>Description</th><th>Price</th><th>Quantity</th><th>Stock-In Date</th></tr>";
+    foreach my $row (@$data) {
+        print "<tr>";
+        foreach my $col (@$row) {
+            print "<td>$col</td>";
+        }
+        print "</tr>";
+    }
+    print "</table>";
+}
